b16c28f50b7f179f502234d03ac8eed47dd66490
hydrogen <ivan.karpov.88@yandex.ru>
2024-01-12 11:59:31 +0700

    create all delete

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ from items_views import router as items_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
