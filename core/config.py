52cea3890fdf8c6e3824e94f83b5db8d4a46e88f
hydrogen <ivan.karpov.88@yandex.ru>
2024-01-12 11:58:08 +0700

    alembic add

--- core/config.py
+++ core/config.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).parent.parent
 class Setting(BaseSettings):
     api_v1_prefix: str = "/api/v1"
     db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    db_echo: bool = True
+    db_echo: bool = False
 
 
 settings = Setting()
